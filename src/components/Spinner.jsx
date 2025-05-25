807d0936770b4dde4d4028437c0a5d5ea84b3dc4
paramsohal05@gmail.com <paramsohal05@gmail.com>
2025-05-25 12:00:34 +0530

    updated react spinner version

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,17 +1,11 @@
-import { ThreeDots } from "react-loader-spinner";
+import {BeatLoader} from 'react-spinners'
+
 const Spinner = ({ text }) => {
   return (
     <div className="flex gap-1 items-center">
       <p className="text-blue-600 font-medium text-lg">{text}</p>
-      <ThreeDots
-        visible={true}
-        height="50"
-        width="50"
-        color="#0047AB"
-        radius="9"
-        ariaLabel="three-dots-loading"
-        wrapperStyle={{}}
-        wrapperClass=""
+      <BeatLoader
+      color="#0047AB" 
       />
     </div>
   );
